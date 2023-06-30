609ad929ea5a23feef64e64349f2da9c70fe0e62
RoyalMamba <ysaurabh059@gmail.com>
2023-06-30 18:33:25 +0530

    Looping Tabs

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+import time
 
 # Set the URL of the meeting
-meeting_url = "https://jiomeetpro.jio.com/shortener?meetingId=5200362798&pwd=rtHt3"
+meeting_url = "https://jiomeetpro.jio.com/shortener?meetingId=6324043443&pwd=r8naH"
 
 # Set the number of guest users to simulate
-num_users = 1
+num_users = 50
 
 # Set the path to the Chrome webdriver (you can use other browsers as well)
 webdriver_path = r"C:\Users\Saurabh16.Yadav\Desktop\jiomeet\chromedriver.exe"
@@ -18,51 +19,39 @@ options = Options()
 # options.add_argument("--headless")      # Run in headless mode, no browser window will be opened
 options.add_argument("--use-fake-ui-for-media-stream")  # Grant permission without user prompt
 options.add_argument("--use-fake-device-for-media-stream")  # Use fake device for media streams
+options.add_argument("--disable-extensions")  # Disable extensions to save resources
+options.add_argument("--disable-gpu")  # Disable GPU to save resources
 
 # Set up the browser driver
-driver = webdriver.Chrome( options=options)
-
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
 try:
-    # Open the initial browser window
+    # Open the initial browser window and join the meeting
     driver.get(meeting_url)
 
-    # Wait for the meeting join page to load
-    WebDriverWait(driver, 10).until(EC.title_contains("JioMeet"))
-
+    # Fill in the form and click the join button
     for i in range(num_users):
-        # Open a new browser window
-        driver.execute_script("window.open('about:blank', 'new_window')")
+        # Wait for the meeting join page to load
+        WebDriverWait(driver, 20).until(EC.title_contains("JioMeet"))
 
-    # Switch to the new windows if they exist
-    if len(driver.window_handles) >= num_users + 1:
-        for i in range(1, num_users + 1):
-            driver.switch_to.window(driver.window_handles[i])
+        # Simulate entering the user's name as a guest
+        name_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "name")))
+        name_field.clear()
+        name_field.send_keys(f"Guest User {i}")
 
-            # Navigate to the meeting URL
-            driver.get(meeting_url)
+        # Click the join button to join the meeting
+        join_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+        join_button.click()
 
-            try:
-                # Wait for the meeting join page to load
-                WebDriverWait(driver, 20).until(EC.title_contains("JioMeet"))
+        # Open a new tab for the next user
+        if i < num_users - 1:
+            driver.execute_script("window.open();")  # Open a new tab
+            driver.switch_to.window(driver.window_handles[i + 1])  # Switch to the new tab
+            driver.get(meeting_url)  # Load the meeting URL
 
-                # Simulate entering the user's name as a guest
-                name_field = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "name")))
-                name_field.clear()
-                name_field.send_keys(f"Guest User {i}")
-
-                # Click the join button to join the meeting
-                join_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/app-root/div/div/div[2]/div/app-join-call/div[1]/div/div[3]/div[2]/div/div/section/form/div[4]")))
-                join_button.click()
-
-                # Wait for the meeting to run (you can add additional logic here, e.g., waiting for specific elements)
-                WebDriverWait(driver, 60).until(EC.title_contains("JioMeet"))
-
-                # Perform additional actions or interactions within the meeting as needed
-
-            except Exception as e:
-                print(f"Error occurred for user {i}: {str(e)}")
-    else:
-        print("Not enough windows available for users")
+    # Stay in the meeting indefinitely without refreshing
+    while True:
+        time.sleep(10)  # Sleep for an interval to keep the script running
 
 finally:
     # Close all browser windows and quit the driver
